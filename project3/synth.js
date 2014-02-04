c4a49af495b0427b6f9fb4a7cd248e4de878c633
Eric Buckthal <ebuckthal@gmail.com>
2014-02-04 11:31:07 -0800

    lowpass bandpass filtering and audio in

--- project3/synth.js
+++ project3/synth.js
@@ -4,17 +4,38 @@ var SYNTH = (function() { "use strict";
    var voices = {};
 
    //GUI stuff
-   var settings = null;
-   var gui = null;
+   var settings;
+   var gui;
 
-   var keyboard = null;
+   var keyboard;
 
    //Effects stuff
-   var masterGain = null;
+   var masterGain;
+   var effectsChain;
+   var filter;
+   var bufferSource = audioContext.createBufferSource();
 
+   initFile();
    initGUI();
    initKeyboard();
    initEffects();
+
+   function initFile(file) {
+
+      var reader = new FileReader();
+
+      reader.onload = function(result) {
+
+         audioContext.decodeAudioData(result.target.result, function(buffer) {
+            bufferSource.buffer = buffer;
+            bufferSource.connect(effectsChain);
+            bufferSource.noteOn(0);
+         });
+      };
+
+      reader.readAsArrayBuffer(file);
+
+   };
    
    function initGUI() {
       
@@ -33,6 +54,10 @@ var SYNTH = (function() { "use strict";
          this.attackTime = 0.1;
          this.decayTime = 0.1;
          this.releaseTime = 0.1;
+
+         this.filterType = 'lowpass';
+         this.filterFreq = 100;
+         this.filterQ = 10;
       };
 
 
@@ -45,16 +70,43 @@ var SYNTH = (function() { "use strict";
       menuOscillator.add(settings, 'releaseTime', 0.01, 1);
 
       var menuLFO = gui.addFolder('Frequency Modulator');
-      menuLFO.add(settings, 'lfoFrequency', 0, 200);
-      menuLFO.add(settings, 'lfoGain', 0, 1);
+      var lfoFreqCtrl = menuLFO.add(settings, 'lfoFrequency', 0, 20);
+      var lfoGainCtrl = menuLFO.add(settings, 'lfoGain', 0, 1);
 
       menuOscillator.open();
       menuLFO.open();
 
       var volController = gui.add(settings, 'masterGain', 0, 1); 
+      var filterFreqCtrl = gui.add(settings, 'filterFreq', 100, 22050);
+      var filterQCtrl = gui.add(settings, 'filterQ', 0, 30);
+      var filterTypeCtrl = gui.add(settings, 'filterType', ['lowpass', 'highpass', 'bandpass']);
 
       volController.onChange(function(value) {
          masterGain.gain.value = value;
+      });
+
+      filterFreqCtrl.onChange(function(value) {
+         filter.frequency.value = value;
+      });
+      
+      filterQCtrl.onChange(function(value) {
+         filter.Q.value = value;
+      });
+
+      filterTypeCtrl.onChange(function(value) {
+         filter.type = value;
+      });
+
+      lfoFreqCtrl.onChange(function(value) {
+         for(var voice in voices) {
+            voices[voice].updateLFOFreq(value);
+         }
+      });
+
+      lfoGainCtrl.onChange(function(value) {
+         for(var voice in voices) {
+            voices[voice].updateLFOGain(value);
+         }
       });
 
    };
@@ -95,12 +147,24 @@ var SYNTH = (function() { "use strict";
    function initEffects() {
 
       masterGain = audioContext.createGain();
+      effectsChain = audioContext.createGain();
+      filter = audioContext.createBiquadFilter();
+
+      effectsChain.gain.value = 1;
+      effectsChain.connect(filter);
+
+      filter.type = settings.filterType;
+      filter.frequency.value = settings.filterFreq;
+      filter.Q.value = settings.filterQ;
+      filter.connect(masterGain);
+
 
       masterGain.gain.value = settings.masterGain;
       masterGain.connect(audioContext.destination);
 
 
    };
+
 
    function Voice(frequency, velocity) {
       var now = audioContext.currentTime;
@@ -117,21 +181,29 @@ var SYNTH = (function() { "use strict";
       this.lfo.frequency.value = settings.lfoFrequency;
       this.lfo.connect(this.lfoGain);
 
-      this.lfoGain.gain.value = this.originalFrequency;
+      this.lfoGain.gain.value = settings.lfoGain * this.originalFrequency / 2;
       this.lfoGain.connect(this.oscillator.frequency);
 
-      //this.oscillator.frequency.value = t;
+      this.oscillator.frequency.value = this.originalFrequency;
       this.oscillator.type = settings.oscillatorWaveform;
       this.oscillator.connect(this.envelope);
 
       this.envelope.gain.setValueAtTime(0, now);
       this.envelope.gain.linearRampToValueAtTime(settings.attackGain, now + settings.attackTime);
       this.envelope.gain.linearRampToValueAtTime(settings.oscillatorGain, now + settings.attackTime + settings.decayTime);
-      this.envelope.connect(masterGain);
+      this.envelope.connect(effectsChain);
 
       this.lfo.start(0);
       this.oscillator.start(0);
    };
+
+   Voice.prototype.updateLFOFreq = function(value) {
+      this.lfo.frequency.value = value;
+   }
+   Voice.prototype.updateLFOGain = function(value) {
+      this.lfoGain.gain.value = value*this.originalFrequency / 2;
+   }
+
 
    Voice.prototype.noteOff = function() {
       var now = audioContext.currentTime;
@@ -146,6 +218,7 @@ var SYNTH = (function() { "use strict";
    };
 
    return {
+      initFile: initFile
    }
 
 })();
